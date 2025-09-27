c286a36de56d6d5bdef95d5d6f41e817bad9f5e8
Benjamin <benjamin630@icloud.com>
2025-09-27 12:52:08 -0700

    change related name profile

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     phone_number = models.IntegerField(max_length=10)
     bank_name = models.CharField(blank=True)
     bank_accountId = models.CharField(default=0)
